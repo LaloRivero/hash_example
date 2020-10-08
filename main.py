d17150d703b88336289ca25912c0fc4a39f087f2
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-10-08 08:59:21 -0600

    MVP of the project

--- main.py
+++ main.py
@@ -14,10 +14,55 @@ import dbconfig
 app = create_app()
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def signin():
     data_form = SignInForm()
     context = {
+        'title':'Sign In',
         'data_form': data_form
     }
+    if data_form.validate_on_submit():
+        username= data_form.username.data
+        password = data_form.password.data
+
+        user = dbconfig.db_users.users.find_one({'username':username})
+
+        if user['password'] == password:
+            return redirect('/success')
+        else:
+            flash('Incorrect Password')
+
     return render_template('signin.html', **context)
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    data_form = SignUpForm()
+    context={
+        'title':'Sign Up',
+        'data_form': data_form,
+    }
+
+    if data_form.validate_on_submit():
+        username = data_form.username.data
+        email = data_form.email.data
+        name = data_form.name.data
+        password = data_form.password.data
+        c_password = data_form.retry_password.data
+
+        if password == c_password:
+            dbconfig.db_users.users.insert_one({
+                'username': username,
+                'email': email,
+                'name' : name,
+                'password' : password,
+            })
+            return redirect('/')
+        else:
+            flash('Passwords do not match')
+
+    return render_template('signup.html', **context)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
